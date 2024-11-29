fbe778d5df59ac6fa7951e223390e32082baa605
Henrique <henriquelopes0611@gmail.com>
2024-11-29 19:04:12 -0300

    add requests for testing the blockchain's API

--- request.py
+++ request.py
@@ -1,12 +1,34 @@
 import requests
 
-# Create a new transaction
+# Create a new transaction and send it to the server
 new_transaction = {
-    'sender': 'Alice',
-    'recipient': 'Bob',
-    'amount': 5,
+    'sender': 'Bob',
+    'recipient': 'Alice',
+    'amount': 4,
 }
-
-# Send the transaction to the server
 response = requests.post('http://localhost:5000/transactions/new', json=new_transaction)
 print(response.json())
+
+new_transaction = {
+    'sender': 'Eva',
+    'recipient': 'Eva',
+    'amount': 10,
+}
+response = requests.post('http://localhost:5000/transactions/new', json=new_transaction)
+print(response.json())
+
+new_transaction = {
+    'sender': 'Bob',
+    'recipient': 'Eva',
+    'amount': 3,
+}
+response = requests.post('http://localhost:5000/transactions/new', json=new_transaction)
+print(response.json())
+
+# # Mine a block
+# response = requests.get('http://localhost:5000/mine')
+# print(response.json())
+
+# # Get the full chain (better visualization at the browser)
+# response = requests.get('http://localhost:5000/chain')
+# print(response.json())
